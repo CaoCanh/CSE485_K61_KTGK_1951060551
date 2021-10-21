e11f172ba89dbffc8b6a93696f8c375acefed6f3
Cao Canh <caocanh2211@gmai.com>
2021-10-21 08:59:46 +0700

    add database

---
+++ database/kiemtra.sql
@@ -0,0 +1,73 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Máy chủ: 127.0.0.1
+-- Thời gian đã tạo: Th10 21, 2021 lúc 03:58 AM
+-- Phiên bản máy phục vụ: 10.4.21-MariaDB
+-- Phiên bản PHP: 8.0.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Cơ sở dữ liệu: `kiemtra`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `blood_donor`
+--
+
+CREATE TABLE `blood_donor` (
+  `bd_id` int(11) NOT NULL,
+  `bd_name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `bd_sex` smallint(1) NOT NULL,
+  `bd_age` int(11) NOT NULL,
+  `bd_bgroup` varchar(2) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `bd_reg_date` timestamp NOT NULL DEFAULT current_timestamp(),
+  `bd_phno` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Đang đổ dữ liệu cho bảng `blood_donor`
+--
+
+INSERT INTO `blood_donor` (`bd_id`, `bd_name`, `bd_sex`, `bd_age`, `bd_bgroup`, `bd_reg_date`, `bd_phno`) VALUES
+(1, 'Nguyễn Thị Ánh', 0, 20, 'A', '2021-10-21 00:37:56', '0123456788'),
+(2, 'Nguyễn Văn Toan', 1, 21, 'A', '2021-10-21 00:37:56', '0123456789'),
+(3, 'Nguyễn Văn Báu', 1, 20, 'O', '2021-10-21 00:41:36', '1234567890'),
+(4, 'Nguyễn Văn Báu', 1, 20, 'O', '2021-10-21 00:42:04', '1234567890');
+
+--
+-- Chỉ mục cho các bảng đã đổ
+--
+
+--
+-- Chỉ mục cho bảng `blood_donor`
+--
+ALTER TABLE `blood_donor`
+  ADD PRIMARY KEY (`bd_id`);
+
+--
+-- AUTO_INCREMENT cho các bảng đã đổ
+--
+
+--
+-- AUTO_INCREMENT cho bảng `blood_donor`
+--
+ALTER TABLE `blood_donor`
+  MODIFY `bd_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
